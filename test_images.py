8c84345cd35b38afd2b5d21d7946e62912bc1f48
wildwatermelon <jiakai.kwong@gmail.com>
2022-05-19 16:52:16 +0800

    update

--- test_images.py
+++ test_images.py
@@ -11,8 +11,10 @@ from src.dataset_const import *
 def get_args():
     parser = argparse.ArgumentParser("You Only Look Once: Unified, Real-Time Object Detection")
     parser.add_argument("--image_size", type=int, default=448, help="The common width and height for all images")
-    parser.add_argument("--conf_threshold", type=float, default=0.35)
-    parser.add_argument("--nms_threshold", type=float, default=0.5)
+    # parser.add_argument("--conf_threshold", type=float, default=0.35)
+    # parser.add_argument("--nms_threshold", type=float, default=0.5)
+    parser.add_argument("--conf_threshold", type=float, default=0.1)
+    parser.add_argument("--nms_threshold", type=float, default=0.1)
     parser.add_argument("--pre_trained_model_type", type=str, choices=["model", "params"], default="model")
     parser.add_argument("--pre_trained_model_path", type=str, default="trained_models/whole_model_trained_yolo_voc")
     parser.add_argument("--input", type=str, default="test_images")
